ea565b1675958ec392a6bbc1f640a940d0fd7690
axm-misawa <misawa@axelmark.co.jp>
2017-02-06 22:56:37 +0900

    Add pyenv provisioning script

---
+++ cookbooks/pyenv/default.rb
@@ -0,0 +1,30 @@
+%w(
+  git
+  python-devel
+  gcc
+  gcc-c++
+  zlib-devel
+  bzip2
+  bzip2-devel
+  readline-devel
+  sqlite
+  sqlite-devel
+  openssl-devel
+).each do |pkg|
+	package pkg do
+		user "root"
+	end
+end
+
+git "/opt/pyenv" do
+	repository "https://github.com/yyuu/pyenv"
+end
+
+file "/etc/profile.d/pyev.sh" do
+	mode "644"
+	content <<-EOL
+export PYENV_ROOT="/opt/pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+	EOL
+end
